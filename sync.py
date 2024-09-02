ff8bc9623d90880b130a785b77b3fd98991bcbfd
Pascal Schade <pascal@zauberzeug.com>
2024-09-02 10:51:47 +0200

    setup sync for robots

--- sync.py
+++ sync.py
@@ -4,19 +4,12 @@ import argparse
 
 from livesync import Folder, sync
 
-# parser = argparse.ArgumentParser(description='Sync local code with robot.')
-# parser.add_argument('robot', help='Robot hostname')
+parser = argparse.ArgumentParser(description='Sync local code with robot.')
+parser.add_argument('robot', help='Robot hostname')
 
-# args = parser.parse_args()
-# touch = 'touch /main.py'
-# , on_change=touch
+args = parser.parse_args()
+touch = 'touch ~/zedxmini/main.py'
+folders = [Folder('.', f'{args.robot}:~/zedxmini', on_change=touch)]
 sync(
-    # Folder('.', 'zauberzeug@192.168.178.27:~/zedxmini'),
-    Folder('.', 'zauberzeug@192.168.1.252:~/zedxmini'),
-
-
-    # Uncomment the following lines to sync additional folders and thereby make them available on the robot:
-    # Folder('../rosys/rosys', f'{args.robot}:~/field_friend/rosys', on_change=touch),
-    # Folder('../nicegui/nicegui', f'{args.robot}:~/field_friend/nicegui', on_change=touch),
-    # Folder('../lizard', f'{args.robot}:~/lizard', on_change=touch),
+    Folder('.', f'{args.robot}:~/zedxmini', on_change=touch)
 )
