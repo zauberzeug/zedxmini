0aa4c9f4a651fe5bd70f711996ea055b768f22af
Jens Ogorek <Jens.ogorek@gmail.com>
2025-02-10 15:45:14 +0100

    foxglove temp

--- zedxmini_ros2_docker/launch.sh
+++ zedxmini_ros2_docker/launch.sh
@@ -6,5 +6,5 @@ source /opt/ros/humble/setup.bash
 # Start ZED wrapper node in the background
 ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zedxm &
 
-# Start the ROS bridge server
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml 
+# Start the Foxglove Bridge server
+ros2 launch foxglove_bridge foxglove_bridge_launch.xml 
