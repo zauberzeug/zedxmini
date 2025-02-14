7b98e533945d5852f690182543d39ae4f69cd408
Jens Ogorek <Jens.ogorek@gmail.com>
2025-02-14 16:20:52 +0100

    updated dockerfile

--- zedxmini_ros1_docker/launch.sh
+++ zedxmini_ros1_docker/launch.sh
@@ -2,14 +2,14 @@
 
 # Source ROS1 environment
 source /opt/ros/noetic/setup.bash
-source /root/catkin_ws/devel/setup.bash
+source /ros_ws/devel/setup.bash
 
 # Start roscore and wait for it to be ready
 roscore &
 sleep 5
 
 # Start Foxglove Bridge in the background
-#roslaunch foxglove_bridge foxglove_bridge.launch
+roslaunch foxglove_bridge foxglove_bridge.launch &
 
 #  Start ZED wrapper node
 roslaunch zed_wrapper zedm.launch
